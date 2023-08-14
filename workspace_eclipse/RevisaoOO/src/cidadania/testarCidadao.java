811d6b3dcb90917016d4ed096ebb28e2232a12e2
AlvaroLucioRibeiro <alvaroluciorib@gmail.com>
2023-08-14 20:03:28 -0300

    Updating Desafio: Herança

---
+++ workspace_eclipse/RevisaoOO/src/cidadania/testarCidadao.java
@@ -0,0 +1,38 @@
+package cidadania;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+class testarCidadao {
+
+	@Test
+	void TestandoCidadao() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entre com o nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Entre com o sexo: ");
+        String sexo = scanner.nextLine();
+
+        System.out.print("Entre com a idade: ");
+        Integer idade = scanner.nextInt();
+
+        System.out.print("Entre com o CPF: ");
+        scanner.nextLine();
+        String cpf = scanner.nextLine();
+
+        Cidadao cidadao = new Cidadao(nome, sexo, idade, cpf);
+
+        System.out.println("Nome: " + cidadao.nome);
+        System.out.println("Sexo: " + cidadao.sexo);
+        System.out.println("Idade: " + cidadao.idade);
+        System.out.println("CPF: " + cidadao.cpf);
+
+        scanner.close();
+    }
+
+}
