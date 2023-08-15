70b4682cc81891f92e2a53139399f03a300dcda1
AlvaroLucioRibeiro <alvaroluciorib@gmail.com>
2023-08-15 10:34:18 -0300

    Updating Interface

--- workspace_eclipse/RevisaoOO/src/contabilidade/ContabilidadeTest.java
+++ workspace_eclipse/RevisaoOO/src/contabilidade/ContabilidadeTest.java
@@ -8,27 +8,27 @@ class ContabilidadeTest {
 
 	@Test
 	void testEmpresaMEI() {
-		Double faturamentoMensal = 1000.0;
+		Double faturamentoMensal = 100.0;
 		EmpresaMEI empresaMEI = new EmpresaMEI(faturamentoMensal);
 
-		System.out.println("Teste EmpresaMEI:");
+		System.out.println("EmpresaMEI:");
 		System.out.println("Valor ISS: " + empresaMEI.getValorISS());
 		System.out.println("Valor IR: " + empresaMEI.getValorIR());
 
 		assertEquals(0.0, empresaMEI.getValorISS());
-		assertEquals(275.0, empresaMEI.getValorIR());
+		assertEquals(27.5, empresaMEI.getValorIR());
 	}
 	
 	@Test
 	void testEmpresaSimples() {
-		Double faturamentoMensal = 1000.0;
+		Double faturamentoMensal = 100.0;
 		EmpresaSimples empresaSimples = new EmpresaSimples(faturamentoMensal);
 
-		System.out.println("Teste EmpresaSimples:");
+		System.out.println("EmpresaSimples:");
 		System.out.println("Valor ISS: " + empresaSimples.getValorISS());
 		System.out.println("Valor IR: " + empresaSimples.getValorIR());
 
-		assertEquals(150.0, empresaSimples.getValorISS());
-		assertEquals(80.0, empresaSimples.getValorIR());
+		assertEquals(15.0, empresaSimples.getValorISS());
+		assertEquals(8.0, empresaSimples.getValorIR());
 	}
 }
