6c74f79e2f6e39885864a93da1f09bafbbba67c4
AlvaroLucioRibeiro <alvaroluciorib@gmail.com>
2023-08-21 20:06:53 -0300

    Updating Exercicio: Array

---
+++ workspace_eclipse/RevisaoJavaAvancado/src/avancado/Animal.java
@@ -0,0 +1,24 @@
+package avancado;
+
+public class Animal {
+	
+	private long id;
+	
+	private String nome;
+
+	public Animal(long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+	
+	
+}
